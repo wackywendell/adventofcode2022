34a783356fba9b5301f10b74b62c4c4a75d632fa
Wendell Smith <wackywendell@gmail.com>
2023-07-28 20:54:30 -0400

    Starting day 3

---
+++ src/problems/day03.rs
@@ -0,0 +1,19 @@
+use std::io::Read;
+
+use super::Solver;
+
+pub struct Day03;
+
+impl Solver for Day03 {
+    fn from_input(_input: impl Read) -> anyhow::Result<Self> {
+        Ok(Day03)
+    }
+
+    fn part_one(&self) -> String {
+        unimplemented!();
+    }
+
+    fn part_two(&self) -> String {
+        unimplemented!()
+    }
+}
--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,5 +1,6 @@
 mod day01;
 mod day02;
+mod day03;
 mod solutions;
 mod template;
 #[cfg(test)]
@@ -13,6 +14,7 @@ pub use solutions::Solver;
 
 pub use day01::Day01;
 pub use day02::Day02;
+pub use day03::Day03;
 
 fn unerr<S: Solver + 'static>(input: impl Read) -> anyhow::Result<Box<dyn Solver>> {
     Ok(Box::new(S::from_input(input).context("Failed to parse")?))
@@ -22,6 +24,7 @@ pub fn solver(day: usize, input: impl Read) -> anyhow::Result<Box<dyn Solver>> {
     Ok(match day {
         1 => unerr::<Day01>(input)?,
         2 => unerr::<Day02>(input)?,
+        3 => unerr::<Day03>(input)?,
         _ => bail!("No solution for day {}", day),
     })
 }
