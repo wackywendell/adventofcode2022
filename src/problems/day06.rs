ed7175b7912178f0009cb88a962030375bdb8234
Wendell Smith <wackywendell@gmail.com>
2023-08-04 23:27:27 -0400

    Finished Day 6

--- src/problems/day06.rs
+++ src/problems/day06.rs
@@ -1,34 +1,68 @@
-use std::io::Read;
+use std::{collections::VecDeque, io::Read, str::FromStr};
 
-use super::{solutions::parse_lines, Solver};
+use log::{debug, info};
 
-pub struct Day06(Vec<i64>);
+use super::Solver;
 
-impl Day06 {
-    pub fn max(&self) -> i64 {
-        self.0.iter().copied().max().unwrap_or_default()
-    }
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Message(String);
 
-    pub fn sum(&self) -> i64 {
-        self.0.iter().copied().sum::<i64>()
+impl FromStr for Message {
+    type Err = <String as FromStr>::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Message(s.to_owned()))
     }
 }
 
+impl Message {
+    pub fn find_no_repeats(&self, duplicates: usize) -> Option<usize> {
+        info!("Checking {}", self.0);
+        let mut seen = VecDeque::with_capacity(duplicates);
+        let mut last_dup: isize = -1;
+        for (ix, c) in self.0.chars().enumerate() {
+            seen.push_back(c);
+            if seen.len() > duplicates {
+                seen.pop_front();
+            }
+
+            for (pix, &p) in seen.iter().rev().skip(1).enumerate() {
+                if p == c {
+                    last_dup = last_dup.max((ix - pix) as isize - 1);
+                    debug!("Found duplicate {c}-{p} at {last_dup} = {ix} - {pix} - 1");
+                    break;
+                }
+            }
+
+            debug!("Finishing {ix}:{c}, with last at {last_dup}");
+            if (last_dup + duplicates as isize) <= (ix as isize) {
+                return Some(ix + 1);
+            }
+        }
+
+        None
+    }
+}
+
+pub struct Day06(Message);
+
 impl Solver for Day06 {
     fn from_input(input: impl Read) -> anyhow::Result<Self> {
-        let items = parse_lines::<i64>(input)?;
+        let mut input = input;
+        let mut buf = String::new();
+        input.read_to_string(&mut buf)?;
 
-        Ok(Day06(items))
+        Ok(Day06(Message(buf.trim().to_owned())))
     }
 
     fn part_one(&self) -> String {
-        self.max();
-        unimplemented!()
+        let ix = self.0.find_no_repeats(4).unwrap_or(self.0 .0.len());
+        format!("{ix}")
     }
 
     fn part_two(&self) -> String {
-        self.sum();
-        unimplemented!()
+        let ix = self.0.find_no_repeats(14).unwrap_or(self.0 .0.len());
+        format!("{ix}")
     }
 }
 
@@ -41,20 +75,40 @@ mod tests {
     use super::*;
 
     const EXAMPLE: &str = r"
-        1
-        9
-        4
+        mjqjpqmgbljsphdztnvjfqwrcgsmlb
+        bvwbjplbgvbhsrlpgdmjqwftvncz
+        nppdvjthqldpwncqszvftbrmjlhg
+        nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+        zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
     ";
 
     #[test]
     fn test_part_one() {
-        let day = Day06::from_input(unindented(EXAMPLE).unwrap().as_bytes()).unwrap();
-        assert_eq!(day.max(), 9);
+        let unexamples = unindented(EXAMPLE).unwrap();
+        let messages = unexamples
+            .lines()
+            .map(|l| Message(l.trim().to_owned()))
+            .collect::<Vec<Message>>();
+        let expected = vec![7, 5, 6, 10, 11];
+
+        for (s, e) in messages.iter().zip(expected) {
+            let g = s.find_no_repeats(4);
+            assert_eq!(g, Some(e), "Analyzing {}: Got {:?}, expected {}", s.0, g, e);
+        }
     }
 
     #[test]
     fn test_part_two() {
-        let day = Day06::from_input(unindented(EXAMPLE).unwrap().as_bytes()).unwrap();
-        assert_eq!(day.sum(), 14);
+        let unexamples = unindented(EXAMPLE).unwrap();
+        let messages = unexamples
+            .lines()
+            .map(|l| Message(l.trim().to_owned()))
+            .collect::<Vec<Message>>();
+        let expected = vec![19, 23, 23, 29, 26];
+
+        for (s, e) in messages.iter().zip(expected) {
+            let g = s.find_no_repeats(14);
+            assert_eq!(g, Some(e), "Analyzing {}: Got {:?}, expected {}", s.0, g, e);
+        }
     }
 }
