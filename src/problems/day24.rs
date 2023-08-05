70297e78f4de5fc9feb08504a97e48c8921addf3
Wendell Smith <wackywendell@gmail.com>
2023-08-05 11:53:10 -0400

    Generated new files

---
+++ src/problems/day24.rs
@@ -0,0 +1,60 @@
+use std::io::Read;
+
+use super::{solutions::parse_lines, Solver};
+
+pub struct Day24(Vec<i64>);
+
+impl Day24 {
+    pub fn max(&self) -> i64 {
+        self.0.iter().copied().max().unwrap_or_default()
+    }
+
+    pub fn sum(&self) -> i64 {
+        self.0.iter().copied().sum::<i64>()
+    }
+}
+
+impl Solver for Day24 {
+    fn from_input(input: impl Read) -> anyhow::Result<Self> {
+        let items = parse_lines::<i64>(input)?;
+
+        Ok(Day24(items))
+    }
+
+    fn part_one(&self) -> String {
+        self.max();
+        unimplemented!()
+    }
+
+    fn part_two(&self) -> String {
+        self.sum();
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use test_log::test;
+
+    use crate::problems::testfns::unindented;
+
+    use super::*;
+
+    const EXAMPLE: &str = r"
+        1
+        9
+        4
+    ";
+
+    #[test]
+    fn test_part_one() {
+        let day = Day24::from_input(unindented(EXAMPLE).unwrap().as_bytes()).unwrap();
+        assert_eq!(day.max(), 9);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let day = Day24::from_input(unindented(EXAMPLE).unwrap().as_bytes()).unwrap();
+        assert_eq!(day.sum(), 14);
+    }
+}
