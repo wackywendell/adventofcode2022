88a9e13ee0bf7ecc671c7b28e7fc9da0d85f0b66
Wendell Smith <wackywendell@gmail.com>
2023-08-27 19:19:51 -0400

    Finisehd day 9

--- src/problems/day09.rs
+++ src/problems/day09.rs
@@ -1,9 +1,9 @@
 use std::{collections::HashSet, io::Read, str::FromStr};
 
-use adventofcode2022::{positions::Value, Compass, Position};
+use adventofcode2022::{Compass, Position};
 use anyhow::{anyhow, bail};
 
-use super::{solutions::parse_lines, Solver};
+use super::Solver;
 
 pub struct Instructions(Vec<(Compass, usize)>);
 
@@ -67,7 +67,7 @@ impl LongRope {
         for ix in 1..self.0.len() {
             let prev = self.0[ix - 1];
             let cur = self.0[ix];
-            let (dx, dy) = cur - prev;
+            let (dx, dy) = prev - cur;
 
             if dx.abs() <= 1 && dy.abs() <= 1 {
                 return ix;
@@ -99,22 +99,24 @@ impl Track {
 
         Track { tail_visited }
     }
+    pub fn follow_long(start: LongRope, instructions: &Instructions) -> Self {
+        let mut tail_visited = HashSet::new();
+        let mut rope = start;
+
+        tail_visited.insert(rope.0.last().copied().unwrap());
+
+        for &(dir, n) in &instructions.0 {
+            for _ in 0..n {
+                rope.step(dir);
+                tail_visited.insert(rope.0.last().copied().unwrap());
+            }
+        }
+
+        Track { tail_visited }
+    }
 }
 
 pub struct Day09(Instructions);
-
-impl Day09 {
-    pub fn max(&self) -> i64 {
-        todo!()
-        // self.0.iter().copied().max().unwrap_or_default()
-    }
-
-    pub fn sum(&self) -> i64 {
-        // self.0.iter().copied().sum::<i64>()
-
-        todo!()
-    }
-}
 
 impl Solver for Day09 {
     fn from_input(input: impl Read) -> anyhow::Result<Self> {
@@ -133,8 +135,9 @@ impl Solver for Day09 {
     }
 
     fn part_two(&self) -> String {
-        self.sum();
-        unimplemented!()
+        let track = Track::follow_long(LongRope::initial(10), &self.0);
+        let visits = track.tail_visited.len();
+        format!("{visits}")
     }
 }
 
@@ -146,7 +149,7 @@ mod tests {
 
     use super::*;
 
-    const EXAMPLE: &str = r"
+    const EXAMPLE1: &str = r"
         R 4
         U 4
         L 3
@@ -155,6 +158,17 @@ mod tests {
         D 1
         L 5
         R 2
+    ";
+
+    const EXAMPLE2: &str = r"
+        R 5
+        U 8
+        L 8
+        D 3
+        R 17
+        D 10
+        L 25
+        U 20
     ";
 
     #[test]
@@ -173,8 +187,24 @@ mod tests {
     }
 
     #[test]
+    fn test_long_rope_step() {
+        let mut rope = LongRope::initial(2);
+        rope.step(Compass::East);
+        assert_eq!(rope, LongRope(vec![Position(1, 0), Position(0, 0)]));
+        rope.step(Compass::East);
+        assert_eq!(rope, LongRope(vec![Position(2, 0), Position(1, 0)]));
+        rope.step(Compass::South);
+        assert_eq!(rope, LongRope(vec![Position(2, 1), Position(1, 0)]));
+        rope.step(Compass::South);
+        assert_eq!(rope, LongRope(vec![Position(2, 2), Position(2, 1)]));
+        rope.step(Compass::West);
+        rope.step(Compass::West);
+        assert_eq!(rope, LongRope(vec![Position(0, 2), Position(1, 2)]));
+    }
+
+    #[test]
     fn test_part_one() {
-        let day = Day09::from_input(unindented(EXAMPLE).unwrap().as_bytes()).unwrap();
+        let day = Day09::from_input(unindented(EXAMPLE1).unwrap().as_bytes()).unwrap();
         let track = Track::follow(Rope::default(), &day.0);
         let visits = track.tail_visited.len();
         assert_eq!(visits, 13);
@@ -182,7 +212,14 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let day = Day09::from_input(unindented(EXAMPLE).unwrap().as_bytes()).unwrap();
-        assert_eq!(day.sum(), 14);
+        let day = Day09::from_input(unindented(EXAMPLE1).unwrap().as_bytes()).unwrap();
+        let track = Track::follow_long(LongRope::initial(2), &day.0);
+        let visits = track.tail_visited.len();
+        assert_eq!(visits, 13);
+
+        let day = Day09::from_input(unindented(EXAMPLE2).unwrap().as_bytes()).unwrap();
+        let track = Track::follow_long(LongRope::initial(10), &day.0);
+        let visits = track.tail_visited.len();
+        assert_eq!(visits, 36);
     }
 }
